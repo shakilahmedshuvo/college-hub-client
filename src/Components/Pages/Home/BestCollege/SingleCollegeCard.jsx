0ffa5a5d3dbcc9a4fc7b21cf1a974cc94ca82e95
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-07-22 23:33:21 +0600

    show the card

--- src/Components/Pages/Home/BestCollege/SingleCollegeCard.jsx
+++ src/Components/Pages/Home/BestCollege/SingleCollegeCard.jsx
@@ -1,9 +1,38 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
+
 const SingleCollegeCard = ({ data }) => {
     const { collegeName, collegeImg, admissionDates, events, research, sports } = data;
-    console.log(data);
     return (
-        <div>
-            
+        <div
+            className="card w-[95%] glass font-bold text-center mx-auto my-10">
+            <figure>
+                <img
+                    className="rounded-lg"
+                    src={collegeImg}
+                    alt="" />
+            </figure>
+            {/* card info */}
+            <div
+                className="card-body text-center mx-auto">
+                <h2
+                    className="card-title mx-auto font-bold">
+                    {collegeName}
+                </h2>
+                <p>
+                   Admission Dates
+                </p>
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div
+                    className="card-actions">
+                    <button
+                        className="btn font-bold btn-wide mx-auto bg-black text-white hover:text-black">
+                        <FaAngleDoubleRight />See Classes
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
