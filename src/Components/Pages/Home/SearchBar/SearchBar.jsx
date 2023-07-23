ab110e6da7b859973227528ac357f769c1128809
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-07-23 19:09:47 +0600

    add search bar styles

--- src/Components/Pages/Home/SearchBar/SearchBar.jsx
+++ src/Components/Pages/Home/SearchBar/SearchBar.jsx
@@ -3,15 +3,15 @@ const SearchBar = () => {
         <div
             className="max-w-7xl mx-auto mt-16 flex justify-end my-1">
             <div
-                className="flex items-end justify-end lg:w-[50%] w-full bg-white rounded-lg px-4 py-2 shadow">
+                className="flex items-end justify-end lg:w-[50%] w-full bg-blue-50 rounded-lg px-4 py-2 shadow">
                 <input
                     type="text"
                     placeholder="Search..."
-                    className="w-full h-10 border rounded-md px-3 focus:outline-none focus:ring focus:border-blue-300"
+                    className="w-full h-10 border rounded-md px-3 focus:outline-none focus:ring focus:border-blue-300 bg-amber-50"
                 // onChange={handleChange}
                 />
                 <button
-                    className="ml-2 px-4 py-2 bg-black text-white rounded-md focus:outline-none"
+                    className="ml-2 px-4 py-2 font-bold rounded-md focus:outline-none bg-gradient-to-r from-yellow-200 from-10% via-sky-200 via-30% to-orange-200 to-90%"
                 // onClick={handleSearchButtonClick}
                 >
                     Search
