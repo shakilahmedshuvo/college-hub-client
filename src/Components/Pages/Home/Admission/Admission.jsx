7267d3167a371d3d0635e3ed3618fd5dbdcf9d0f
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-07-23 20:02:28 +0600

    get the data and post the data in the mongodb

--- src/Components/Pages/Home/Admission/Admission.jsx
+++ src/Components/Pages/Home/Admission/Admission.jsx
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 import SectionTitle from "../../../Shared/SectionTitle";
 import useAuth from "../../../Utilities/Hooks/useAuth";
 import { LiaGraduationCapSolid } from "react-icons/lia";
@@ -5,13 +6,56 @@ import { LiaGraduationCapSolid } from "react-icons/lia";
 const Admission = () => {
     const { user } = useAuth();
 
+    // handleBookServices function
+    const handleAdmission = event => {
+        // stop the form reload
+        event.preventDefault();
+
+        // get the value form the filed
+        const form = event.target;
+        const name = form.name.value;
+        const subject = form.subject.value;
+        const email = user?.email;
+        const phone = form.phone.value;
+        const address = form.address.value;
+        const date = form.date.value;
+        const imgURL = form.imgURL.value;
+
+        // get all data
+        const allData = { name, subject, email, phone, address, date, imgURL };
+
+        // data post to the backend server( mongodb )
+        fetch('http://localhost:5000/admission', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(allData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    // swal 
+                    Swal.fire({
+                        position: 'top-center',
+                        icon: 'success',
+                        title: 'Your Admission has been Successfully',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    form.reset();
+                }
+            })
+    };
+
     return (
         <div
-            className="bg-indigo-50 pt-20">
+            className="bg-indigo-50">
             <div
-                className="max-w-7xl mx-auto">
+                className="max-w-7xl mx-auto pt-20">
                 <div>
-                    <LiaGraduationCapSolid className="mx-auto text-6xl mt-14" />
+                    <LiaGraduationCapSolid className="mx-auto text-6xl" />
                     <SectionTitle
                         heading={"Admission"}
                         subHeading={"Secure your future with us. Explore our comprehensive programs, experienced faculty, and a supportive learning environment. Join us for an enriching educational journey."}
@@ -24,9 +68,9 @@ const Admission = () => {
                 {/* form section start */}
                 <form
                     className="font-bold mt-6 pb-16"
-                // onSubmit={handleBookService}
-                >
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    onSubmit={handleAdmission}>
+                    <div
+                        className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div className="form-control">
                             <label className="label">
                                 <span
@@ -37,7 +81,8 @@ const Admission = () => {
                             <input
                                 type="text"
                                 name="name"
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -49,7 +94,8 @@ const Admission = () => {
                             <input
                                 type="text"
                                 name="subject"
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -62,7 +108,8 @@ const Admission = () => {
                                 type="text"
                                 name="email"
                                 defaultValue={user?.email}
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -74,7 +121,8 @@ const Admission = () => {
                             <input
                                 type="number"
                                 name="phone"
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -86,7 +134,8 @@ const Admission = () => {
                             <input
                                 type="text"
                                 name="address"
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -98,12 +147,22 @@ const Admission = () => {
                             <input
                                 type="date"
                                 name="date"
-                                className="input input-bordered" />
+                                className="input input-bordered"
+                                required />
                         </div>
-                        <input
-                            type="file"
-                            name="image"
-                            className="file-input file-input-bordered file-input-warning w-full max-w-xs" />
+                        <div className="form-control">
+                            <label className="label">
+                                <span
+                                    className="label-text">
+                                    *Image URL
+                                </span>
+                            </label>
+                            <input
+                                type="text"
+                                name="imgURL"
+                                className="input input-bordered"
+                                required />
+                        </div>
                     </div>
                     <div className="form-control mt-6">
                         <input
