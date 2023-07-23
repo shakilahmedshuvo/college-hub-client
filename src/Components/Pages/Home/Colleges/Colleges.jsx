b09bf4186a95f3200ea5c334e3861e423ea5dfb6
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-07-23 14:14:09 +0600

    add best six colleges

--- src/Components/Pages/Home/Colleges/Colleges.jsx
+++ src/Components/Pages/Home/Colleges/Colleges.jsx
@@ -1,7 +1,18 @@
 import { FaSchool } from "react-icons/fa";
 import SectionTitle from "../../../Shared/SectionTitle";
+import { useEffect, useState } from "react";
+import SingleCollege from "./SingleCollege";
 
 const Colleges = () => {
+    // load the data
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/bestCollege')
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+            })
+    }, []);
 
     return (
         <div
@@ -12,6 +23,16 @@ const Colleges = () => {
                     heading={"Our Popular Colleges"}
                     subHeading={"Top colleges offering diverse programs, excellent faculty, and vibrant campus life.Renowned colleges with strong academic traditions, modern facilities, and thriving student communities."}
                 ></SectionTitle>
+            </div>
+            {/* best college card map section */}
+            <div
+                className="grid lg:grid-cols-3 md:grid-cols-2 gap-3 mx-auto">
+                {
+                    data.map(data => <SingleCollege
+                        key={data.id}
+                        data={data}
+                    ></SingleCollege>)
+                }
             </div>
         </div>
     );
---
+++ src/Components/Pages/Home/Colleges/SingleCollege.jsx
@@ -0,0 +1,47 @@
+import { PiStarDuotone } from "react-icons/pi";
+import { FcCalendar, FcSearch } from "react-icons/fc";
+
+const SingleCollege = ({ data }) => {
+    const { collegeName, collegeRatings, admissionDate, research, collegeImg } = data;
+
+    return (
+        <div
+            className="card w-[95%] glass font-bold text-center mx-auto my-10">
+            <figure>
+                <img
+                    className="rounded-lg"
+                    src={collegeImg}
+                    alt="College Image" />
+            </figure>
+            {/* card info */}
+            <div
+                className="card-body text-center mx-auto">
+                <h2
+                    className="card-title mx-auto font-bold">
+                    {collegeName}
+                </h2>
+                <p
+                    className="flex items-center justify-center">
+                    <FcCalendar className="mr-2 text-2xl" /> Admission Dates: {admissionDate}
+                </p>
+                <p
+                    className="flex items-center justify-center">
+                    < FcSearch className="mr-1 text-xl mt-1" /> Research History Count: {research}
+                </p>
+                <p
+                    className="flex items-center justify-center">
+                    <PiStarDuotone className="mr-2 text-yellow-500 text-xl" /> College Ratings: {collegeRatings}
+                </p>
+                <div
+                    className="card-actions">
+                    <button
+                        className="btn font-bold btn-wide mx-auto bg-black text-white hover:text-black">
+                        See Details
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleCollege;
